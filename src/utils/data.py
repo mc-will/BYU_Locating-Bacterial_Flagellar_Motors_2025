e9fbceb6a72bd65120b0696d502a69536603ecd2
Thomas Denoirjean <128964980+ThomasDenoirjean@users.noreply.github.com>
2025-06-02 17:36:19 +0200

    updating the select_tomo_ids function of src/utils/data.py

--- src/utils/data.py
+++ src/utils/data.py
@@ -1,13 +1,13 @@
 import pandas as pd
 
-def select_tomo_ids(df, number_of_slices=[300], max_number_of_motors=1, y_shape_range=(924, 960), x_shape_range=(924, 960)) -> pd.Series:
+def select_tomo_ids(df, number_of_slices=[300], number_of_motors=[0, 1], y_shape_range=(924, 960), x_shape_range=(924, 960)) -> pd.Series:
     '''
     Return the list of the tomo_ids obtained by filtering the DataFrame base on the given parameters
 
             Parameters:
                     df (pd.Dataframe): the dataset to filter
                     number_of_slices (list:int): number of slices per tomogram
-                    max_number_of_motors (int): max number of motors
+                    max_number_of_motors (list:int): max number of motors
                     y_shape_range(tuple:int): tuple of the (min, max) y size of pictures
                     x_shape_range(tuple:int): tuple of the (min, max) x size of pictures
 
@@ -16,7 +16,7 @@ def select_tomo_ids(df, number_of_slices=[300], max_number_of_motors=1, y_shape_
     '''
     df = df[(df['Array_shape_axis_1'] >= y_shape_range[0]) & (df['Array_shape_axis_2'] <= y_shape_range[1])]
     df = df[(df['Array_shape_axis_1'] >= x_shape_range[0]) & (df['Array_shape_axis_2'] <= x_shape_range[1])]
-    df = df[(df['Array_shape_axis_0'].isin(number_of_slices)) & (df['Number_of_motors'] <= max_number_of_motors)]
+    df = df[(df['Array_shape_axis_0'].isin(number_of_slices)) & (df['Number_of_motors'].isin(number_of_motors))]
 
 
     return df.tomo_id
