449875e9b1a7c36228b188d12f28e4fd7c02ae30
Benedetta Veronesi <benedetta1.veronesi@gmail.com>
2025-06-04 16:10:01 +0200

    packaged functions to preprocess and read data in batches, updated function to plot history reg, updated notebook

--- src/utils/model_plot.py
+++ src/utils/model_plot.py
@@ -2,9 +2,15 @@ import matplotlib.pyplot as plt
 
 
 
-
 def plot_history_regression(history, xlims=None, ylims=None):
-    # Compute default limits based on history if not provided
+    '''
+    Function to plot learning curves for a regression task
+    Parameters:
+        history: output from a model.fit
+        xlims,ylims (tuple of float, optional): limits for x and y axes, if not
+                                                provided are defined as
+                                                (0,max(epochs)), (0,max(loss))
+    '''
     if xlims is None:
         xlims = (0, max(history.epoch))
     if ylims is None:
@@ -16,11 +22,15 @@ def plot_history_regression(history, xlims=None, ylims=None):
     ax[0].plot(history.epoch, history.history["val_loss"], label="Validation loss")
     ax[0].set_ylim(ylims)
     ax[0].set_xlim(xlims)
+    ax[0].set_xlabel('Epochs')
+    ax[0].set_ylabel('Loss')
 
     ax[1].set_title('mse')
     ax[1].plot(history.epoch, history.history["mse"], label="Train mse")
     ax[1].plot(history.epoch, history.history["val_mse"], label="Validation mse")
     ax[1].set_ylim(ylims)
     ax[1].set_xlim(xlims)
+    ax[1].set_xlabel('Epochs')
+    ax[1].set_ylabel('MSE')
     ax[0].legend()
     ax[1].legend()
