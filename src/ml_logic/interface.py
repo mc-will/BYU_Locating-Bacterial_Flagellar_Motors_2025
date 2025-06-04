8cf089289a10b752f27d6f43db42cfd0c25e4483
Thomas Denoirjean <128964980+ThomasDenoirjean@users.noreply.github.com>
2025-06-04 15:32:36 +0200

    added docstring to train (mlflow logic)

--- src/ml_logic/interface.py
+++ src/ml_logic/interface.py
@@ -16,15 +16,39 @@ from src.ml_logic.registry import mlflow_run, mlflow_transition_model
 def train(model,
         X_train,
         y_train,
-        model_type,
-        preprocess_type,
-        model_name,
+        model_type: str,
+        preprocess_type: str,
+        model_name: str,
         validation_data=None, # overrides validation_split
         validation_split=0.3,
         batch_size = 256,
         patience = 2
     ) -> float:
+    '''
+    Train a compiled model and saves it with mlflow on a server. Dependant on .env file to get the uri of the server,
+    and to define the experiment name. The function asks for different string parameters to ensure a good monitoring of
+    models produced.
 
+            Parameters:
+                    model: an initialized and compiled model
+                    X_train: pictures array
+                    y_train: could be any type of y (2D/3D position, true/false, etc.)
+                    model_type (str): for monitoring purpose, the type of the model: 2D/3D, position or presence
+                    preprocess_type (str): for monitoring purpose, the kind of preprocessed images used (mean, equaladaptX, best_slice)
+                    model_name (str): for monitoring purpose, the name of the model, ideally a combinaition of the two previous parameters
+                    validation_data: validation data, not necessary if validation split is given
+                    validation_split: fraction used for the validation
+                    batch_size: batch size
+                    patience: patience
+
+            Example:
+                If you are running a model working on 2D picture to identify the position of motors, selecting the best
+                slice of the tomogram, then call the function with model_type='pos2D', preprocess_type='best_slice',
+                model_name='reg on x,y - best_slice':
+
+                `from src.ml_logic.interface import train
+                train(model, X_train, y_train, 'pos2D', 'best_slice', 'reg on x,y - best_slice')`
+    '''
     print(Fore.GREEN + "\n🏋️ Starting model training ..." + Style.RESET_ALL)
 
     model, history = train_model(
